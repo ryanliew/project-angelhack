5d7eceb224fa9eeb0f5babe21c7deedd56b2e391
Ryan Liew <fatal-fates@hotmail.com>
2016-06-04 15:06:01 +0800

    Added all initial classes and database classes

---
+++ app/src/main/java/anglehack/grabngo/Constants.java
@@ -0,0 +1,13 @@
+package anglehack.grabngo;
+
+/**
+ * Created by SLW2 on 6/4/2016.
+ */
+public class Constants {
+    public static String dateFormat()
+    {
+        return "ddMMyy H:m";
+    }
+
+
+}
